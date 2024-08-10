9e9138216320520eef0095dd7abccd79a3f3d7a0
lazarus. <62876764+artisanbk@users.noreply.github.com>
2024-08-10 15:51:44 +0800

    Add files via upload

---
+++ neovim/lua/lazarus/plugins/lsp/mason.lua
@@ -0,0 +1,37 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim", -- auto install linters and all that when neovim is booted up
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+    -- local mason_tool_installer = require("mason-tool-installer") -- useful for first time installs
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "asm_lsp",        -- assembly
+        "autotools_ls",   -- automake, Make
+        "ltex",           -- LaTeX
+        "lua_ls",         -- Lua
+        "markdown_oxide", -- Markdown
+        "sqls",           -- SQL
+        "taplo",          -- TOML
+      },
+    })
+  end
+}
